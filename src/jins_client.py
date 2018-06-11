3183615ae9563a5b7d94a433a09b96ebc15eaff2
Chris Raff <crharfifs@gmail.com>
2018-06-11 16:28:59 -0400

    jins_client: print format is readable

--- src/jins_client.py
+++ src/jins_client.py
@@ -21,12 +21,20 @@ def read_forever():
 		new_char = s.recv(1)
 		read_in_buffer += new_char
 
+		# if there was a newline, write the frame
 		if read_in_buffer[-2:] == b'\r\n':
-			newest_frame = read_in_buffer[:-2].decode('utf-8')
-			history += [newest_frame]
-			print(newest_frame)
+			new_frame_raw = read_in_buffer[:-2].decode('utf-8')
+			new_frame = parse_frame(new_frame_raw)
+			history += [new_frame]
+			print('\t'.join(new_frame))
 
 			read_in_buffer = b''
+
+def parse_frame(frame):
+	# ignore the initial 'artifact' field since it is always empty
+	elements = frame[1:].split(',')
+
+	return elements
 
 if __name__ == '__main__':
 	host = ''
