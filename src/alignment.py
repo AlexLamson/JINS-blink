f331e7e942b63faa83d3bbb8491ded792a520def
Alex Lamson <alex.lamson@gmail.com>
2018-06-15 18:21:16 -0400

    Alignment script works now

--- src/alignment.py
+++ src/alignment.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from merge_data import preprocess_dataframes, combine_data
 from sklearn import preprocessing
+import pickle
 
 '''
 read in jins data
@@ -21,14 +22,15 @@ loop until you close out of the graph before completing the second click
 
 
 if __name__ == '__main__':
-    # jins_fname = '../res/data1/jins_20180612174521.csv'
-    # openface_fname = '../res/data1/webcam_2018-06-12-13-45.csv'
-    # output_fname = '../res/data1/combined.csv'
-    jins_fname = '../res/data4/28A18305A891_20180612181409.csv'
-    openface_fname = '../res/data4/webcam_2018-06-12-14-12.csv'
-    output_fname = '../res/data4/combined.csv'
+    jins_fname = '../res/data1/jins_20180612174521.csv'
+    openface_fname = '../res/data1/webcam_2018-06-12-13-45.csv'
+    output_fname = '../res/data1/combined.csv'
+    # jins_fname = '../res/data4/28A18305A891_20180612181409.csv'
+    # openface_fname = '../res/data4/webcam_2018-06-12-14-12.csv'
+    # output_fname = '../res/data4/combined.csv'
 
     preview_size_in_seconds = 1*60  # how much time of the beginning should we save, by number of samples?
+
 
     print('loading jins data')
     jins_df = pd.read_csv(jins_fname, skiprows=5, nrows=100*preview_size_in_seconds)
@@ -40,16 +42,17 @@ if __name__ == '__main__':
     jins_df, openface_df = preprocess_dataframes(jins_df, openface_df)
 
     print('interpolating openface frames')
-    combine_data(jins_df, openface_df, delay=0)
+    combine_data(jins_df, openface_df)
     combined_df = jins_df
 
-    print('flatten the eog values to make it easier to read')
-    combined_df['EOG_V'] = combined_df['EOG_V'].abs()
+    # print('flatten the eog values to make it easier to read')
+    # combined_df['EOG_V'] = combined_df['EOG_V'] - 0.5
 
-    print('normalize eog values')
+    print('normalize & shift eog values')
     eog_v = combined_df[['EOG_V']].values.astype(float)
     min_max_scaler = preprocessing.MinMaxScaler()
     eog_v_normalized = min_max_scaler.fit_transform(eog_v)
+    eog_v_normalized = eog_v_normalized - 0.5
 
     print('normalize au45 values')
     au45_r = combined_df[['AU45_r']].values.astype(float)
@@ -57,29 +60,25 @@ if __name__ == '__main__':
     au45_r_normalized = min_max_scaler.fit_transform(au45_r)
 
     print("="*30)
-    print("click the blue point, then the red point")
+    print("Your last 2 clicks should be on the blue point then the red point. Then you can close the window.")
     print("="*30)
 
     # Simple mouse click function to store coordinates
     def onclick(event):
+        # figure out if this is a right click (later)
+
         global ix, iy
         ix, iy = event.xdata, event.ydata
 
-        print("{:.3f}, {:.3f}".format(ix, iy))
+        # print("{:.3f}, {:.3f}".format(ix, iy))
 
         # assign global variable to access outside of function
         global coords
         coords.append((ix, iy))
 
-        # Disconnect after 2 clicks
-        if len(coords) == 2:
-            fig.canvas.mpl_disconnect(cid)
-            plt.close(1)
-        return
-
     coords = []
 
-    # show a plot of the normalized data
+    print("plotting normalized data")
     fig = plt.figure(1)
     ax = fig.add_subplot(111)
     x = combined_df[['TIME']].values.astype(float)
@@ -88,12 +87,21 @@ if __name__ == '__main__':
     cid = fig.canvas.mpl_connect('button_press_event', onclick)  # add callback function
     plt.show()
 
+    fig.canvas.mpl_disconnect(cid)
+    print("this is very important")
+
     # get the coordinates where the user clicked
-    jins_x = coords[0][0]
-    openface_x = coords[1][0]
-    print("coords: {:.4f} {:.4f}".format(jins_x, openface_x))
+    jins_x = coords[-2][0]
+    openface_x = coords[-1][0]
+    print("coords: {:.3f} {:.3f}".format(jins_x, openface_x))
 
     # determine the index of the each selected point
-    jins_start_frame = combined_df.ix[(combined_df['TIME']-jins_x).abs().argsort()[:2]].iloc[0].name
-    openface_start_frame = combined_df.ix[(combined_df['TIME']-openface_x).abs().argsort()[:2]].iloc[0].name
-    print("jins_start_frame: {} openface_start_frame: {}".format(jins_start_frame, openface_start_frame))
+    jins_start_time = combined_df.ix[(combined_df['TIME']-jins_x).abs().argsort()[:2]].iloc[0]['TIME']
+    openface_start_time = combined_df.ix[(combined_df['TIME']-openface_x).abs().argsort()[:2]].iloc[0]['TIME']
+    print("jins_start_time: {:.3f} openface_start_time: {:.3f}".format(jins_start_time, openface_start_time))
+
+    start_times_filename = "start_times.pickle"
+    print("writing starting frames to {}".format(start_times_filename))
+    with open(start_times_filename, 'wb') as f:
+        start_times = (jins_start_time, openface_start_time)
+        pickle.dump(start_times, f)
--- src/merge_data.py
+++ src/merge_data.py
@@ -1,38 +1,58 @@
-
-
 import pandas as pd
 import numpy as np
+import pickle
+from tqdm import tqdm
+from sklearn import preprocessing
 
 
 # combines jins and openface frames into the jins dataframe.
-def combine_data(jins_df, of_df, delay):
-    jins_frame = 0
+def combine_data(jins_df, of_df):
+    jins_frame = jins_df.iloc[0].name
     jins_size_full = jins_df.shape[0]
+    end_of_time = of_df['TIME'].iloc[-1]
 
-    # find jins frame based on delay
-    while jins_df['TIME'][jins_frame] < delay:
-        jins_df.drop(jins_frame, inplace=True)
-        jins_frame += 1
+    with tqdm(total=jins_size_full) as pbar:
+        while jins_frame < jins_size_full and jins_df['TIME'].iloc[jins_frame] <= end_of_time:
+            for column in of_df.columns:
+                if column == 'TIME':
+                    continue
 
-    last_of_time = of_df['timestamp'][of_df.shape[0] - 1]
-    while jins_size_full > jins_frame and jins_df['TIME'][jins_frame] <= last_of_time:
-        for column in of_df.columns:
-            if column == 'timestamp':
-                continue
+                # interpolate the value of the column according to jins time
+                jins_df.at[jins_frame, column] = np.interp(jins_df['TIME'].iloc[jins_frame], of_df['TIME'], of_df[column])
 
-            # interpolate the value of the column according to jins time
-            jins_df.at[jins_frame, column] = np.interp(jins_df['TIME'][jins_frame] - delay, of_df['timestamp'], of_df[column])
-
-        jins_frame += 1
+            pbar.update(1)
+            jins_frame += 1
 
     return jins_df
 
+# # combines jins and openface frames into the jins dataframe.
+# def combine_data(jins_df, of_df, delay=0):
+#     jins_frame = 0
+#     jins_size_full = jins_df.shape[0]
+
+#     # find jins frame based on delay
+#     while jins_df['TIME'][jins_frame] < delay:
+#         jins_df.drop(jins_frame, inplace=True)
+#         jins_frame += 1
+
+#     last_of_time = of_df['timestamp'][of_df.shape[0] - 1]
+#     while jins_size_full > jins_frame and jins_df['TIME'][jins_frame] <= last_of_time:
+#         for column in of_df.columns:
+#             if column == 'timestamp':
+#                 continue
+
+#             # interpolate the value of the column according to jins time
+#             jins_df.at[jins_frame, column] = np.interp(jins_df['TIME'][jins_frame] - delay, of_df['timestamp'], of_df[column])
+
+#         jins_frame += 1
+
+#     return jins_df
 
 # turns the jins time column into relative times
 def time_column_to_delta(jins_df):
     # find the resolution by taking the difference of the first two timestamps
-    first_time = float( jins_df['TIME'][0] [-3:] )
-    second_time = float( jins_df['TIME'][1] [-3:] )
+    first_time = float( jins_df['TIME'].iloc[0][-3:] )
+    second_time = float( jins_df['TIME'].iloc[1][-3:] )
     res = second_time - first_time
 
     # the data is evenly spaced, so just apply the res time all the way down
@@ -44,6 +64,7 @@ def preprocess_dataframes(jins_df, openface_df):
 
     jins_df.rename( columns={'DATE': 'TIME'}, inplace=True )  # rename the date column to time so it makes sense
     openface_df.columns = openface_df.columns.str.strip()  # columns have leading space, get rid of it
+    openface_df.rename( columns={'timestamp': 'TIME'}, inplace=True )  # rename the timestamp column so it's consistent
 
     # cull openface data
     # keep time, AU45_r (blink regression)
@@ -60,18 +81,36 @@ def preprocess_dataframes(jins_df, openface_df):
 
     time_column_to_delta(jins_df)
 
+    # print('normalize jins data')
+    # x = jins_df.values
+    # min_max_scaler = preprocessing.MinMaxScaler()
+    # x_scaled = min_max_scaler.fit_transform(x)
+    # jins_df = pd.DataFrame(x_scaled, columns=jins_df.columns)
+
+    # print('normalize openface data')
+    # x = openface_df.values
+    # min_max_scaler = preprocessing.MinMaxScaler()
+    # x_scaled = min_max_scaler.fit_transform(x)
+    # openface_df = pd.DataFrame(x_scaled, columns=openface_df.columns)
+
     return jins_df, openface_df
 
 
-if __name__ == '__main__':
-    # jins_fname = '../res/data1/jins_20180612174521.csv'
-    # openface_fname = '../res/data1/webcam_2018-06-12-13-45.csv'
-    # output_fname = '../res/data1/combined.csv'
-    jins_fname = '../res/data4/28A18305A891_20180612181409.csv'
-    openface_fname = '../res/data4/webcam_2018-06-12-14-12.csv'
-    output_fname = '../res/data4/combined.csv'
+def trim_by_start_time(df, start_time):
+    df = df.drop(df[df['TIME'] < start_time].index)
+    df['TIME'] = df['TIME'] - df['TIME'].values[0]
+    fresh_index = np.array(list(range(df.shape[0])))
+    df.set_index(fresh_index, inplace=True)
+    return df
 
-    openface_delay = 2.0  # seconds between the time JINS began recording and openface began recording
+
+if __name__ == '__main__':
+    jins_fname = '../res/data1/jins_20180612174521.csv'
+    openface_fname = '../res/data1/webcam_2018-06-12-13-45.csv'
+    output_fname = '../res/data1/combined.csv'
+    # jins_fname = '../res/data4/28A18305A891_20180612181409.csv'
+    # openface_fname = '../res/data4/webcam_2018-06-12-14-12.csv'
+    # output_fname = '../res/data4/combined.csv'
 
     print('loading jins data')
     jins_df = pd.read_csv(jins_fname, skiprows=5)
@@ -82,8 +121,19 @@ if __name__ == '__main__':
     print('preprocessing dataframes')
     jins_df, openface_df = preprocess_dataframes(jins_df, openface_df)
 
-    print('combining')
-    combine_data(jins_df, openface_df, openface_delay)
+    print('aligning data')
+    start_times_filename = "start_times.pickle"
+    (jins_start_time, openface_start_time) = pickle.load(open(start_times_filename, "rb"))
+    print("starting frames: jins {:.3f}  openface {:.3f}".format(jins_start_time, openface_start_time))
+    openface_df = trim_by_start_time(openface_df, openface_start_time)
+    jins_df = trim_by_start_time(jins_df, jins_start_time)
+
+    print('saving cleaned data')
+    jins_df.to_csv('../res/data1/jins_clean.csv')
+    openface_df.to_csv('../res/data1/openface_clean.csv')
+
+    print('combining (may be slow for first 10-15 seconds)')
+    combine_data(jins_df, openface_df)
 
     print('saving combined data')
     jins_df.to_csv(output_fname)
