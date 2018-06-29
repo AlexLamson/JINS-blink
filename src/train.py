1653e39a6406aecaf2f82d5cf9111b8af9b7d1f5
Alex Lamson <alex.lamson@gmail.com>
2018-06-29 14:48:42 -0400

    add some regression models to train.py

--- src/train.py
+++ src/train.py
@@ -1,10 +1,10 @@
 # trains a model to identify blinks
-
+from util import *
 import pickle
 import pandas as pd
 import numpy as np
 from tqdm import tqdm
-from sklearn import cross_validation
+from sklearn.model_selection import KFold
 from sklearn.metrics import confusion_matrix, precision_recall_fscore_support
 
 
@@ -24,20 +24,24 @@ def extract_features(window):
     out = np.append( out, np.max(magnitudes) )
     out = np.append( out, out[-1] - out[-2] )  # max - min of magnitudes
     out = np.append( out, magnitudes[-1] - magnitudes[0] )  # diff from start to end magnitude
-    
+
     return out
 
 
-def evaluate_model(model, inputs, outputs):
-    n = len(inputs)
+def evaluate_model(model, inputs, outputs, is_classification=True):
+    if is_classification:
+        print("evaluating classification model")
+    else:
+        print("evaluating regression model")
 
-    cv = cross_validation.KFold(n, n_folds=10, shuffle=True, random_state=None)
+    cv = KFold(n_splits=10, shuffle=True, random_state=None)
 
     # used for averaging the confusion matrices, fscores
     conf_array = []
     f_array = []
+    loss_array = []
 
-    for i, (train_indices, test_indices) in enumerate(cv):
+    for i, (train_indices, test_indices) in enumerate(cv.split(inputs)):
         # split into training and testing
         inputs_train = inputs[train_indices, :]
         outputs_train = outputs[train_indices]
@@ -48,40 +52,46 @@ def evaluate_model(model, inputs, outputs):
 
         predictions = model.predict(inputs_test)
 
-        conf_array += [ confusion_matrix(outputs_test, predictions) ]
-        _, _, fscore, _ = precision_recall_fscore_support(outputs_test, predictions)
-        f_array += [ fscore ]
+        if is_classification:
+            conf_array += [ confusion_matrix(outputs_test, predictions) ]
+            _, _, fscore, _ = precision_recall_fscore_support(outputs_test, predictions)
+            f_array += [ fscore ]
+        else:
+            l1_loss = abs(outputs_test-predictions)
+            loss_array += [ np.mean(l1_loss) ]
 
-    print()
-    print('predicted\n open\tblink')
-    print( np.mean(conf_array, axis=0) )
-    print('fscore', np.mean(f_array, axis=0))
+    if is_classification:
+        print('predicted\n open\tblink')
+        print( np.mean(conf_array, axis=0) )
+        print('fscore', np.mean(f_array, axis=0))
+    else:
+        print("average L1 loss: {}".format( np.mean(l1_loss) ))
 
 
-def slidingWindow(sequence,winSize,step=1):
+def slidingWindow(sequence,winSize,stride=1):
     """Returns a generator that will iterate through
     the defined chunks of input sequence.  Input sequence
     must be iterable.
     @author: snoran
     Thanks to https://scipher.wordpress.com/2010/12/02/simple-sliding-window-iterator-in-python/"""
- 
+
     # Verify the inputs
-    try: 
+    try:
         it = iter(sequence)
     except TypeError:
         raise Exception("**ERROR** sequence must be iterable.")
-    if not ((type(winSize) == type(0)) and (type(step) == type(0))):
-        raise Exception("**ERROR** type(winSize) and type(step) must be int.")
-    if step > winSize:
-        raise Exception("**ERROR** step must not be larger than winSize.")
+    if not ((type(winSize) == type(0)) and (type(stride) == type(0))):
+        raise Exception("**ERROR** type(winSize) and type(stride) must be int.")
+    if stride > winSize:
+        raise Exception("**ERROR** stride must not be larger than winSize.")
     if winSize > len(sequence):
         raise Exception("**ERROR** winSize must not be larger than sequence length.")
- 
+
     # Pre-compute number of chunks to emit
-    numOfChunks = int((len(sequence)-winSize)/step)+1
-    
+    numOfChunks = int((len(sequence)-winSize)/stride)+1
+
     # Do the work
-    for i in range(0,numOfChunks*step,step):
+    for i in range(0,numOfChunks*stride,stride):
         yield i, sequence[i:i+winSize]
 
 
@@ -100,7 +110,8 @@ def extract_features_from_dataframe(df, window_size):
             features = extract_features(window)
 
             inputs += [ features ]
-            outputs += [ np.mean(window_full[:, -1]) >= 0.6 ]  # turn the blink data into booleans
+            # outputs += [ np.mean(window_full[:, -1]) >= 0.6 ]  # turn the blink data into booleans
+            outputs += [ window_full[int(window_full.shape[0]/2), -1] ]  # leave the blink regression as-is
 
             pbar.update(1)
 
@@ -120,25 +131,46 @@ if __name__ == '__main__':
     data = pd.read_csv(training_data_fname)
     print('done loading')
 
-    
-    print('extracting features')
-    inputs, outputs = extract_features_from_dataframe(data, window_size)
+    features_pickle_filename = data_folder+"features.pickle"
+    if file_exists(features_pickle_filename):
+        print('loading precomputed features')
+        inputs, outputs = load_obj(features_pickle_filename)
+    else:
+        print('extracting features & saving to file')
+        inputs, outputs = extract_features_from_dataframe(data, window_size)
+        save_obj((inputs, outputs), features_pickle_filename)
+
+
+    # uncomment line to sample it so the models train faster
+    seconds_of_data = 60*10
+    inputs, outputs = inputs[:seconds_of_data*100], outputs[:seconds_of_data*100]
+
 
     # train the model
-    print('evaluating')
+    """
+    Regression models
+    """
+    # from sklearn.linear_model import LinearRegression
+    # model = LinearRegression()
+    # from sklearn.tree import DecisionTreeRegressor
+    # model = DecisionTreeRegressor(max_depth=3)
+    # evaluate_model(model, inputs, outputs, False)
 
+    """
+    Classification models
+    """
+    outputs = outputs >= 0.6
+    # from sklearn.linear_model import LogisticRegression
+    # model = LogisticRegression()
     # from sklearn.tree import DecisionTreeClassifier
-    # model = DecisionTreeClassifier(criterion='entropy', max_depth=3)
-    # from sklearn import svm
-    # model = svm.SVC()
+    # model = DecisionTreeClassifier(max_depth=3)
+    # from sklearn.svm import LinearSVC
+    # model = LinearSVC()
     from sklearn.neighbors import KNeighborsClassifier
     model = KNeighborsClassifier(n_neighbors=3, weights='distance')
     evaluate_model(model, inputs, outputs)
 
     # save the model
-    # be sure to re-train the best model on the full data-set
-    print('final fit')
-    model.fit(inputs, outputs)
-    print('saving')
-    with open('model.pickle', 'wb') as f:
-        pickle.dump(model, f)
+    print('fitting whole dataset')
+    model.fit(inputs, outputs)  # be sure to re-train the best model on the full data-set
+    save_obj(model, 'model.pickle')
