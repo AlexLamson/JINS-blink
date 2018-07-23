aefb7b6c6e88768de136ae27f48df5f0c5465c34
Alex Lamson <alex.lamson@gmail.com>
2018-07-23 14:07:29 -0400

    made open_face_time_adjuster.py more modular

--- src/loading_multi_subject_data/open_face_time_adjuster.py
+++ src/loading_multi_subject_data/open_face_time_adjuster.py
@@ -1,22 +1,31 @@
+#!/usr/bin/python3
+import sys
+sys.path.append('..')
+from util import *
+
 # replace the timestamp column with a matlab vector
 
-import numpy as np
+# import numpy as np
 import pandas as pd
 import scipy.io
 
 
-def main():
-    mat_fname = '../Data_Experiment_W!NCE/101/label0/time_stamps/101_label0_1.mat'
-    open_face_fname = '../Data_Experiment_W!NCE/101/label0/oface/101_label0_1.csv'
-    output_fname = '../Data_Experiment_W!NCE/101/label0/oface/101_label0_1_correct_times.csv'
+def create_corrected_file(openface_path, mat_path, output_path):
 
-    mat = scipy.io.loadmat(mat_fname)['time_stamps'].flatten()
-    data = pd.read_csv(open_face_fname)
+    mat = scipy.io.loadmat(mat_path)['time_stamps'].flatten()
+    data = pd.read_csv(openface_path)
 
     data.iloc[:, 1] = mat
 
-    data.to_csv(output_fname)
+    data.to_csv(output_path)
 
 
 if __name__ == "__main__":
-	main()
+    subject_id, label_id = 101, "label0"
+
+    openface_path = 'C:/Data_Experiment_W!NCE/{0}/FACS/{1}/oface/{0}_{1}.csv'.format(subject_id, label_id)
+    mat_path = 'C:/Data_Experiment_W!NCE/{0}/FACS/{1}/time_stamps/{0}_{1}.mat'.format(subject_id, label_id)
+    output_path = 'C:/Data_Experiment_W!NCE/{0}/FACS/{1}/oface/{0}_{1}_correct_times.csv'.format(subject_id, label_id)
+
+    # create_corrected_file(openface_path, mat_path, output_path)
+    guarantee_execution(create_corrected_file, (openface_path, mat_path, output_path))
