a925a27e6bf8f7a110f16fce8d2e30597654ae48
Alex Lamson <alex.lamson@gmail.com>
2018-09-05 11:08:56 -0400

    add more code to load openface data

--- src/extract_blink_windows/extract_blinks.py
+++ src/extract_blink_windows/extract_blinks.py
@@ -24,6 +24,7 @@ data_filename = "all_data_raw.pkl"
 subject_numbers = [101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117]
 label_numbers = [1, 2, 3, 4, 5]
 label_names = "None,Brow lower,Brow raiser,Cheek raiser,Nose wrinkler".split(',')
+of_time_per_jins_time = 1.00610973512302
 '''
 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 '''
@@ -55,7 +56,26 @@ if __name__ == '__main__':
     print(raw_index.shape)
 
     # load the openface data
-    # create functions to translate the timestamps between the two
+    for subject_number in subject_numbers:
+        for label_number in label_numbers:
+            openface_path = get_path(subject_number, label_number)
+            if not file_exists(openface_path):
+                print("MISSING "+openface_path)
+                continue
+            openface_df = get_data(openface_path)
+            # print(openface_df.shape)
+            # exit()
+            # pass
+
+
+            start_times_dict_string = '{}_label{}'.format(subject_number, label_number)
+            oface_start, jins_start = start_times_dict[start_times_dict_string]
+            oface_start, jins_start = oface_start-1, jins_start-1  # convert to zero-indexed
+
+            # create functions to translate the timestamps between the two
+
+            exit()
+
 
     # for each subject
     #     for each label
