0b54da05c44c847ac2a9be2d104016b210fa1f2f
Alex Lamson <alex.lamson@gmail.com>
2018-09-10 11:20:08 -0400

    improved plots

--- src/extract_blink_windows/extract_blinks.py
+++ src/extract_blink_windows/extract_blinks.py
@@ -59,6 +59,10 @@ if __name__ == '__main__':
     for subject_number in subject_numbers:
         for label_number in label_numbers:
 
+            # if label_number < 2 or subject_number < 103:
+            #     continue
+
+
             # get the start times
             start_times_dict_string = '{}_label{}'.format(subject_number, label_number)
             if start_times_dict_string not in start_times_dict:
@@ -106,6 +110,49 @@ if __name__ == '__main__':
             jins_df['AU45_r'] = pd.Series(interpolated_openface_data, index=jins_df.index)
             combined_df = jins_df
 
+
+            scale_factor = 10  # just for visualization purposes
+            # combined_df['AU45_r'] = np.gradient(combined_df['AU45_r'])*scale_factor
+            threshold = thresholds[start_times_dict_string]
+            print("threshold is {}".format(threshold))
+
+            
+            # mask = combined_df['AU45_r'] > threshold
+            # combined_df['AU45_r'][mask] = 5
+            # combined_df['AU45_r'][np.invert(mask)] = 0
+
+
+            # combined_df['AU45_r'] = np.gradient(combined_df['AU45_r'])
+            # blink_starts = np.argwhere(combined_df['AU45_r'] > 0).flatten()[0::2]
+            # blink_ends = np.argwhere(combined_df['AU45_r'] < 0).flatten()[0::2]
+            # blink_pairs = np.array(list(zip(blink_starts, blink_ends)))
+            # blink_indicies = np.mean(blink_pairs, axis=1).astype(int)
+            # # print(blink_indicies)
+            # # print(blink_starts)
+            # # print(blink_ends)
+
+            # combined_df['peaks'] = np.zeros(len(jins_df['AU45_r']))
+            # combined_df['peaks'][blink_indicies] = 5
+
+
+            # exit()
+
+
+
+
+            # import peakutils
+            # indexes = peakutils.indexes(combined_df['AU45_r'], thres=threshold, min_dist=15)
+
+            # new_column = np.zeros(len(jins_df['AU45_r']))
+            # new_column[indexes] = 5
+            # jins_df['peaks'] = pd.Series(new_column, index=jins_df.index)
+
+
+
+            # print(a)
+            # exit()
+
+
             # a=combined_df['AU45_r']
             # print(a.shape)
             # a=np.gradient(combined_df['AU45_r'])
@@ -119,9 +166,25 @@ if __name__ == '__main__':
             '''
             # TODO: normalize the data
 
-            combined_df['EOG_V'] /= np.std(combined_df['EOG_V'])
-            plt.plot(combined_df['TIME'], combined_df['EOG_V'])
-            plt.plot(combined_df['TIME'], combined_df['AU45_r'])
+            # print(combined_df)
+            # exit()
+
+
+            # combined_df['EOG_V'] /= np.std(combined_df['EOG_V'])
+            # plt.plot(combined_df['TIME'], combined_df['EOG_V'])
+            # plt.plot(combined_df['TIME'], combined_df['peaks'])
+            # plt.plot(combined_df['TIME'], combined_df['AU45_r'])
+
+            # combined_df['peaks'] = combined_df['peaks'] * 500
+            combined_df['AU45_r'] = combined_df['AU45_r'] * 500
+
+            plt.plot(combined_df['frame'], combined_df['EOG_L'])
+            plt.plot(combined_df['frame'], combined_df['EOG_R'])
+            plt.plot(combined_df['frame'], combined_df['EOG_H'])
+            plt.plot(combined_df['frame'], combined_df['EOG_V'])
+            plt.plot(combined_df['frame'], combined_df['AU45_r'])
+            # plt.plot(combined_df['frame'], combined_df['peaks'])
+
             plt.show()
 
 
